85d367789b8b9cadca98a669b682cd06a7fc72ba
Aigars Silkalns <silkalns@Aigarss-iMac.local>
2016-02-27 23:28:21 +0200

    Updated ECharts

--- production/js/echart/green.js
+++ production/js/echart/green.js
@@ -1,10 +1,10 @@
 var theme = {
-                    // 默认色板
+
                     color: [
                         '#26B99A', '#34495E', '#BDC3C7', '#3498DB',
                         '#9B59B6', '#8abb6f', '#759c6a', '#bfd3b7'
                     ],
-                    // 图表标题
+
                     title: {
                         itemGap: 8,
                         textStyle: {
@@ -12,57 +12,57 @@ var theme = {
                             color: '#408829'
                         }
                     },
-                    // 值域
+
                     dataRange: {
                         color: ['#1f610a', '#97b58d']
                     },
-                    // 工具箱
+
                     toolbox: {
                         color: ['#408829', '#408829', '#408829', '#408829']
                     },
-                    // 提示框
+
                     tooltip: {
                         backgroundColor: 'rgba(0,0,0,0.5)',
-                        axisPointer: {// 坐标轴指示器，坐标轴触发有效
-                            type: 'line', // 默认为直线，可选为：'line' | 'shadow'
-                            lineStyle: {// 直线指示器样式设置
+                        axisPointer: {
+                            type: 'line',
+                            lineStyle: {
                                 color: '#408829',
                                 type: 'dashed'
                             },
                             crossStyle: {
                                 color: '#408829'
                             },
-                            shadowStyle: {// 阴影指示器样式设置
+                            shadowStyle: {
                                 color: 'rgba(200,200,200,0.3)'
                             }
                         }
                     },
-                    // 区域缩放控制器
+
                     dataZoom: {
-                        dataBackgroundColor: '#eee', // 数据背景颜色
-                        fillerColor: 'rgba(64,136,41,0.2)', // 填充颜色
-                        handleColor: '#408829'     // 手柄颜色
+                        dataBackgroundColor: '#eee',
+                        fillerColor: 'rgba(64,136,41,0.2)',
+                        handleColor: '#408829'
                     },
                     grid: {
                         borderWidth: 0
                     },
-                    // 类目轴
+
                     categoryAxis: {
-                        axisLine: {// 坐标轴线
-                            lineStyle: {// 属性lineStyle控制线条样式
+                        axisLine: {
+                            lineStyle: {
                                 color: '#408829'
                             }
                         },
-                        splitLine: {// 分隔线
-                            lineStyle: {// 属性lineStyle（详见lineStyle）控制线条样式
+                        splitLine: {
+                            lineStyle: {
                                 color: ['#eee']
                             }
                         }
                     },
-                    // 数值型坐标轴默认参数
+
                     valueAxis: {
-                        axisLine: {// 坐标轴线
-                            lineStyle: {// 属性lineStyle控制线条样式
+                        axisLine: {
+                            lineStyle: {
                                 color: '#408829'
                             }
                         },
@@ -72,8 +72,8 @@ var theme = {
                                 color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
                             }
                         },
-                        splitLine: {// 分隔线
-                            lineStyle: {// 属性lineStyle（详见lineStyle）控制线条样式
+                        splitLine: {
+                            lineStyle: {
                                 color: ['#eee']
                             }
                         }
@@ -87,16 +87,16 @@ var theme = {
                             emphasis: {color: '#408829'}
                         }
                     },
-                    // K线图默认参数
+
                     k: {
                         itemStyle: {
                             normal: {
-                                color: '#68a54a', // 阳线填充颜色
-                                color0: '#a9cba2', // 阴线填充颜色
+                                color: '#68a54a',
+                                color0: '#a9cba2',
                                 lineStyle: {
                                     width: 1,
-                                    color: '#408829', // 阳线边框颜色
-                                    color0: '#86b379'   // 阴线边框颜色
+                                    color: '#408829',
+                                    color0: '#86b379'
                                 }
                             }
                         }
@@ -113,7 +113,7 @@ var theme = {
                                     }
                                 }
                             },
-                            emphasis: {// 也是选中样式
+                            emphasis: {
                                 areaStyle: {
                                     color: '#99d2dd'
                                 },
@@ -166,28 +166,28 @@ var theme = {
                     gauge: {
                         startAngle: 225,
                         endAngle: -45,
-                        axisLine: {// 坐标轴线
-                            show: true, // 默认显示，属性show控制显示与否
-                            lineStyle: {// 属性lineStyle控制线条样式
+                        axisLine: {
+                            show: true,
+                            lineStyle: {
                                 color: [[0.2, '#86b379'], [0.8, '#68a54a'], [1, '#408829']],
                                 width: 8
                             }
                         },
-                        axisTick: {// 坐标轴小标记
-                            splitNumber: 10, // 每份split细分多少段
-                            length: 12, // 属性length控制线长
-                            lineStyle: {// 属性lineStyle控制线条样式
+                        axisTick: {
+                            splitNumber: 10,
+                            length: 12,
+                            lineStyle: {
                                 color: 'auto'
                             }
                         },
-                        axisLabel: {// 坐标轴文本标签，详见axis.axisLabel
-                            textStyle: {// 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                        axisLabel: {
+                            textStyle: {
                                 color: 'auto'
                             }
                         },
-                        splitLine: {// 分隔线
-                            length: 18, // 属性length控制线长
-                            lineStyle: {// 属性lineStyle（详见lineStyle）控制线条样式
+                        splitLine: {
+                            length: 18,
+                            lineStyle: {
                                 color: 'auto'
                             }
                         },
@@ -196,17 +196,17 @@ var theme = {
                             color: 'auto'
                         },
                         title: {
-                            textStyle: {// 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                            textStyle: {
                                 color: '#333'
                             }
                         },
                         detail: {
-                            textStyle: {// 其余属性默认使用全局文本样式，详见TEXTSTYLE
+                            textStyle: {
                                 color: 'auto'
                             }
                         }
                     },
                     textStyle: {
-                        fontFamily: '微软雅黑, Arial, Verdana, sans-serif'
+                        fontFamily: 'Arial, Verdana, sans-serif'
                     }
                 }
