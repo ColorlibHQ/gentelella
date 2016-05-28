45e4883e27645a6caf44800e5997a1ef35b759b6
christianesperar <christianesperar@gmail.com>
2016-05-29 01:20:52 +0800

    Add Browsersync and Autoprefixer on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 var DEST = 'build/';
 
@@ -12,14 +14,17 @@ gulp.task('scripts', function() {
       .pipe(gulp.dest(DEST+'/js'))
       .pipe(rename({suffix: '.min'}))
       .pipe(uglify())
-      .pipe(gulp.dest(DEST+'/js'));
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(browserSync.stream());
 });
 
 // TODO: Maybe we can simplify how sass compile the minify and unminify version
 var compileSASS = function (filename, options) {
   return sass('src/scss/*.scss', options)
+        .pipe(autoprefixer('last 2 versions', '> 5%'))
         .pipe(concat(filename))
-        .pipe(gulp.dest(DEST+'/css'));
+        .pipe(gulp.dest(DEST+'/css'))
+        .pipe(browserSync.stream());
 };
 
 gulp.task('sass', function() {
@@ -30,12 +35,23 @@ gulp.task('sass-minify', function() {
     return compileSASS('custom.min.css', {style: 'compressed'});
 });
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+        startPath: './production/index.html'
+    });
+});
+
 gulp.task('watch', function() {
-   // Watch .js files
+  // Watch .html files
+  gulp.watch('production/*.html', browserSync.reload);
+  // Watch .js files
   gulp.watch('src/js/*.js', ['scripts']);
-   // Watch .scss files
+  // Watch .scss files
   gulp.watch('src/scss/*.scss', ['sass', 'sass-minify']);
 });
 
 // Default Task
-gulp.task('default', ['watch']);
+gulp.task('default', ['browser-sync', 'watch']);
