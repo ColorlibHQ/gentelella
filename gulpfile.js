a099f4592ed30a4bc9f9170b60dbde6e567f1dfd
Mohammad Reza Pazooki <ipazooki@live.com>
2016-12-02 16:58:48 +0330

    RTL Widgets

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('browser-sync', function() {
         server: {
             baseDir: './'
         },
-        startPath: './production/index.html'
+        startPath: './production/index-rtl.html'
     });
 });
 
