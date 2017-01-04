ef2b5f0ede64c68aa8805e3cb528d380ac412d59
Nicolae Gabriel <nicolae.andrei.cristian@gmail.com>
2017-01-04 16:50:22 +0200

    #286
    Sidebar with "nav-sm" on current page always display child menu
    
    - Solved it.

--- build/js/new-custom2.js
+++ build/js/new-custom2.js
@@ -69,28 +69,34 @@ var setContentHeight = function () {
 	$RIGHT_COL.css('min-height', contentHeight);
 };
 
-$SIDEBAR_MENU.find('a').on('click', function(ev) {
-	var $li = $(this).parent();
+  $SIDEBAR_MENU.find('a').on('click', function(ev) {
+        var $li = $(this).parent();
 
-	if ($li.is('.active')) {
-		$li.removeClass('active active-sm');
-		$('ul:first', $li).slideUp(function() {
-			setContentHeight();
-		});
-	} else {
-		// prevent closing menu if we are on child menu
-		if (!$li.parent().is('.child_menu')) {
-			$SIDEBAR_MENU.find('li').removeClass('active active-sm');
-			$SIDEBAR_MENU.find('li ul').slideUp();
-		}
-		
-		$li.addClass('active');
+        if ($li.is('.active')) {
+            $li.removeClass('active active-sm');
+            $('ul:first', $li).slideUp(function() {
+                setContentHeight();
+            });
+        } else {
+            // prevent closing menu if we are on child menu
+            if (!$li.parent().is('.child_menu')) {
+                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                $SIDEBAR_MENU.find('li ul').slideUp();
+            }else
+            {
+				if ( $BODY.is( ".nav-sm" ) )
+				{
+					$SIDEBAR_MENU.find( "li" ).removeClass( "active active-sm" );
+					$SIDEBAR_MENU.find( "li ul" ).slideUp();
+				}
+			}
+            $li.addClass('active');
 
-		$('ul:first', $li).slideDown(function() {
-			setContentHeight();
-		});
-	}
-});
+            $('ul:first', $li).slideDown(function() {
+                setContentHeight();
+            });
+        }
+    });
 
 // toggle small or large menu
 $MENU_TOGGLE.on('click', function() {
