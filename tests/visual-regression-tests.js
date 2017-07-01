754f1a6f065bb369e2160daaa8fc38129719b7f6
Holmes Conan <holmesconan@gmail.com>
2017-07-01 10:27:08 +0800

    Update tests.

--- tests/visual-regression-tests.js
+++ tests/visual-regression-tests.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var phantomcss = require('phantomcss');
 
 function _onPass(test) {
@@ -17,35 +18,31 @@ casper.test.begin('Gentelella visual tests', function (test) {
     rebase: casper.cli.get("rebase"),
     // SlimerJS needs explicit knowledge of this Casper, and lots of absolute paths
     casper: casper,
-    libraryRoot: fs.absolute(fs.workingDirectory + ''),
-    screenshotRoot: fs.absolute(fs.workingDirectory + '/screenshots'),
-    failedComparisonsRoot: fs.absolute(fs.workingDirectory + '/failures'),
+    libraryRoot: fs.absolute(fs.workingDirectory + '/node_modules/phantomcss'),
+    screenshotRoot: fs.absolute(fs.workingDirectory + '/tests/screenshots'),
+    failedComparisonsRoot: fs.absolute(fs.workingDirectory + '/tests/failures'),
     addLabelToFailedImage: true,
 
     /*
-    screenshotRoot: '/screenshots',
-    failedComparisonsRoot: '/failures'
-    casper: specific_instance_of_casper,
-    libraryRoot: '/phantomcss',
     fileNameGetter: function overide_file_naming(){},
     onPass: function passCallback(){},
     onFail: function failCallback(){},
     onTimeout: function timeoutCallback(){},
-      onComplete: function completeCallback(){},
-      hideElements: '#thing.selector',
-      addLabelToFailedImage: true,
-      outputSettings: {
-        errorColor: {
-          red: 255,
-          green: 255,
-          blue: 0
-        },
-        errorType: 'movement',
-        transparency: 0.3
-      }
-      */
+    onComplete: function completeCallback(){},
+    hideElements: '#thing.selector',
+    outputSettings: {
+      errorColor: {
+        red: 255,
+        green: 255,
+        blue: 0
+      },
+      errorType: 'movement',
+      transparency: 0.3
+    }
+    */
   });
 
+  // CasperJS error handlers.
   casper.on('remote.message', function (msg) {
     this.echo(msg);
   });
@@ -57,53 +54,42 @@ casper.test.begin('Gentelella visual tests', function (test) {
   });
 
   // The test scenario
+  // Start test suite
   casper.start();
   casper.viewport(1920, 1080);
-  casper.thenOpen('http://localhost:3000/./production/index.html', function () {
+
+  // Test screenshot: index.html.
+  casper.open('http://localhost:3000/./production/index.html', function () {
     phantomcss.screenshot('body', 'index');
   });
 
+  // // index.html user-profile dropdown.
   casper.then(function () {
-    casper.click('.user-profile.dropdown-toggle');
+    casper.click('.user-profile');
+
     // wait for modal to fade-in
     casper.waitForSelector('li.open .user-profile.dropdown-toggle',
       function success() {
         phantomcss.screenshot('body', 'index-with-user-profile-dropdown');
       },
       function timeout() {
-        casper.test.fail('Should see coffee machine');
+        casper.test.fail('Should see ');
       }
     );
   });
-  casper.then(function () {
-    casper.click('#cappuccino-button');
-    phantomcss.screenshot('#myModal', 'cappuccino success');
+
+  // Test screenshot: login.html login-form.
+  casper.thenOpen('http://localhost:3000/./production/index.html', function () {
+    phantomcss.screenshot('body', 'login-form');
   });
-  casper.then(function () {
-    casper.click('#close');
-    // wait for modal to fade-out
-    casper.waitForSelector('#myModal[style*="display: none"]',
-      function success() {
-        phantomcss.screenshot({
-          'Coffee machine close success': {
-            selector: '#coffee-machine-wrapper',
-            ignore: '.selector'
-          },
-          'Coffee machine button success': '#coffee-machine-button'
-        });
-      },
-      function timeout() {
-        casper.test.fail('Should be able to walk away from the coffee machine');
-      }
-    );
-  });
+
+  // Check all screenshots.
   casper.then(function now_check_the_screenshots() {
     // compare screenshots
     phantomcss.compareAll();
   });
-  /*
-  Casper runs tests
-  */
+
+  // Casper runs tests
   casper.run(function () {
     console.log('\nTHE END.');
     // phantomcss.getExitStatus() // pass or fail?
