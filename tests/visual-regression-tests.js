5f184c7414e276066b4e6850d627fbaae8380725
Holmes Conan <holmesconan@gmail.com>
2017-07-01 14:05:19 +0800

    Update visual tests script.

--- tests/visual-regression-tests.js
+++ tests/visual-regression-tests.js
@@ -22,24 +22,6 @@ casper.test.begin('Gentelella visual tests', function (test) {
     screenshotRoot: fs.absolute(fs.workingDirectory + '/tests/screenshots'),
     failedComparisonsRoot: fs.absolute(fs.workingDirectory + '/tests/failures'),
     addLabelToFailedImage: true,
-
-    /*
-    fileNameGetter: function overide_file_naming(){},
-    onPass: function passCallback(){},
-    onFail: function failCallback(){},
-    onTimeout: function timeoutCallback(){},
-    onComplete: function completeCallback(){},
-    hideElements: '#thing.selector',
-    outputSettings: {
-      errorColor: {
-        red: 255,
-        green: 255,
-        blue: 0
-      },
-      errorType: 'movement',
-      transparency: 0.3
-    }
-    */
   });
 
   // CasperJS error handlers.
@@ -73,15 +55,209 @@ casper.test.begin('Gentelella visual tests', function (test) {
         phantomcss.screenshot('body', 'index-with-user-profile-dropdown');
       },
       function timeout() {
-        casper.test.fail('Should see ');
+        casper.test.fail('Should see user profile dropdown.');
       }
     );
   });
 
   // Test screenshot: login.html login-form.
-  casper.thenOpen('http://localhost:3000/./production/index.html', function () {
+  casper.thenOpen('http://localhost:3000/./production/login.html', function () {
     phantomcss.screenshot('body', 'login-form');
   });
+
+  // Test screenshot: login.html register-form.
+  casper.then(function () {
+    casper.click('.to_register');
+
+    // wait for modal to fade-in
+    casper.wait(1000,
+      function done() {
+        phantomcss.screenshot('body', 'register-form');
+      }
+    );
+  });
+
+  // Test screenshot: calendar.html
+  casper.thenOpen('http://localhost:3000/./production/calendar.html', function () {
+    phantomcss.screenshot('body', 'calendar');
+  });
+
+  // Test screenshot: chartjs.html
+  casper.thenOpen('http://localhost:3000/./production/chartjs.html', function () {
+    phantomcss.screenshot('body', 'chartjs');
+  });
+
+  // Test screenshot: contacts.html
+  casper.thenOpen('http://localhost:3000/./production/contacts.html', function () {
+    phantomcss.screenshot('body', 'contacts');
+  });
+
+  // Test screenshot: echarts.html
+  casper.thenOpen('http://localhost:3000/./production/echarts.html', function () {
+    phantomcss.screenshot('body', 'echarts');
+  });
+
+  // Test screenshot: e_commerce.html
+  casper.thenOpen('http://localhost:3000/./production/e_commerce.html', function () {
+    phantomcss.screenshot('body', 'e_commerce');
+  });
+
+  // Test screenshot: fixed_footer.html
+  casper.thenOpen('http://localhost:3000/./production/fixed_footer.html', function () {
+    phantomcss.screenshot('body', 'fixed_footer');
+  });
+
+  // Test screenshot: fixed_sidebar.html
+  casper.thenOpen('http://localhost:3000/./production/fixed_sidebar.html', function () {
+    phantomcss.screenshot('body', 'fixed_sidebar');
+  });
+
+  // Test screenshot: form_advanced.html
+  casper.thenOpen('http://localhost:3000/./production/form_advanced.html', function () {
+    phantomcss.screenshot('body', 'form_advanced');
+  });
+
+  // Test screenshot: form_buttons.html
+  casper.thenOpen('http://localhost:3000/./production/form_buttons.html', function () {
+    phantomcss.screenshot('body', 'form_buttons');
+  });
+
+  // Test screenshot: form.html
+  casper.thenOpen('http://localhost:3000/./production/form.html', function () {
+    phantomcss.screenshot('body', 'form');
+  });
+
+  // Test screenshot: form_upload.html
+  casper.thenOpen('http://localhost:3000/./production/form_upload.html', function () {
+    phantomcss.screenshot('body', 'form_upload');
+  });
+
+  // Test screenshot: form_validation.html
+  casper.thenOpen('http://localhost:3000/./production/form_validation.html', function () {
+    phantomcss.screenshot('body', 'form_validation');
+  });
+
+  // Test screenshot: form_wizards.html
+  casper.thenOpen('http://localhost:3000/./production/form_wizards.html', function () {
+    phantomcss.screenshot('body', 'form_wizards');
+  });
+
+  // Test screenshot: general_elements.html
+  casper.thenOpen('http://localhost:3000/./production/general_elements.html', function () {
+    phantomcss.screenshot('body', 'general_elements');
+  });
+
+  // Test screenshot: glyphicons.html
+  casper.thenOpen('http://localhost:3000/./production/glyphicons.html', function () {
+    phantomcss.screenshot('body', 'glyphicons');
+  });
+
+  // Test screenshot: icons.html
+  casper.thenOpen('http://localhost:3000/./production/icons.html', function () {
+    phantomcss.screenshot('body', 'icons');
+  });
+
+  // Test screenshot: inbox.html
+  casper.thenOpen('http://localhost:3000/./production/inbox.html', function () {
+    phantomcss.screenshot('body', 'inbox');
+  });
+
+  // Test screenshot: invoice.html
+  casper.thenOpen('http://localhost:3000/./production/invoice.html', function () {
+    phantomcss.screenshot('body', 'invoice');
+  });
+
+  // Test screenshot: map.html
+  casper.thenOpen('http://localhost:3000/./production/map.html', function () {
+    phantomcss.screenshot('body', 'map');
+  });
+
+  // Test screenshot: media_gallery.html
+  casper.thenOpen('http://localhost:3000/./production/media_gallery.html', function () {
+    phantomcss.screenshot('body', 'media_gallery');
+  });
+
+  // Test screenshot: morisjs.html
+  casper.thenOpen('http://localhost:3000/./production/morisjs.html', function () {
+    phantomcss.screenshot('body', 'morisjs');
+  });
+
+  // Test screenshot: other_charts.html
+  casper.thenOpen('http://localhost:3000/./production/other_charts.html', function () {
+    phantomcss.screenshot('body', 'other_charts');
+  });
+
+  // Test screenshot: page_403.html
+  casper.thenOpen('http://localhost:3000/./production/page_403.html', function () {
+    phantomcss.screenshot('body', 'page_403');
+  });
+
+  // Test screenshot: page_404.html
+  casper.thenOpen('http://localhost:3000/./production/page_404.html', function () {
+    phantomcss.screenshot('body', 'page_404');
+  });
+
+  // Test screenshot: page_500.html
+  casper.thenOpen('http://localhost:3000/./production/page_500.html', function () {
+    phantomcss.screenshot('body', 'page_500');
+  });
+
+  // Test screenshot: plain_page.html
+  casper.thenOpen('http://localhost:3000/./production/plain_page.html', function () {
+    phantomcss.screenshot('body', 'plain_page');
+  });
+
+  // Test screenshot: pricing_tables.html
+  casper.thenOpen('http://localhost:3000/./production/pricing_tables.html', function () {
+    phantomcss.screenshot('body', 'pricing_tables');
+  });
+
+  // Test screenshot: profile.html
+  casper.thenOpen('http://localhost:3000/./production/profile.html', function () {
+    phantomcss.screenshot('body', 'profile');
+  });
+
+  // Test screenshot: project_detail.html
+  casper.thenOpen('http://localhost:3000/./production/project_detail.html', function () {
+    phantomcss.screenshot('body', 'project_detail');
+  });
+
+  // Test screenshot: projects.html
+  casper.thenOpen('http://localhost:3000/./production/projects.html', function () {
+    phantomcss.screenshot('body', 'projects');
+  });
+
+  // Test screenshot: projects.html
+  casper.thenOpen('http://localhost:3000/./production/projects.html', function () {
+    phantomcss.screenshot('body', 'projects');
+  });
+
+  // Test screenshot: tables.html
+  casper.thenOpen('http://localhost:3000/./production/tables.html', function () {
+    phantomcss.screenshot('body', 'tables');
+  });
+
+  // Test screenshot: tables_dynamic.html
+  casper.thenOpen('http://localhost:3000/./production/tables_dynamic.html', function () {
+    phantomcss.screenshot('body', 'tables_dynamic');
+  });
+
+  // Test screenshot: typography.html
+  casper.thenOpen('http://localhost:3000/./production/typography.html', function () {
+    phantomcss.screenshot('body', 'typography');
+  });
+
+  // Test screenshot: widgets.html
+  casper.thenOpen('http://localhost:3000/./production/widgets.html', function () {
+    phantomcss.screenshot('body', 'widgets');
+  });
+
+  // Test screenshot: widgets.html
+  casper.thenOpen('http://localhost:3000/./production/widgets.html', function () {
+    phantomcss.screenshot('body', 'widgets');
+  });
+
+
 
   // Check all screenshots.
   casper.then(function now_check_the_screenshots() {
@@ -96,3 +272,13 @@ casper.test.begin('Gentelella visual tests', function (test) {
     casper.test.done();
   });
 });
+
+/**
+ * Skipped pages:
+ * production/xx.html
+ * production/chartjs2.html
+ * production/index2.html
+ * production/index3.html
+ * production/level2.html
+ */
+
