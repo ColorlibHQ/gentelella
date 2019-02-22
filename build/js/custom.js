c6753042a6fcf8f9a2d3e833d28d9d5d197e9200
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-02-22 17:42:49 +0200

    responsive menu functionality fix

--- build/js/custom.js
+++ build/js/custom.js
@@ -124,8 +124,8 @@ $(document).ready(function () {
 
             $('.main_container > .row > .left_col').removeClass('col-md-2 col-lg-2').addClass('col-md-1 col-lg-1 col-2');
             if ($('.main_container > .row > .left_col.menu_fixed').length) {
-                if ($(window).width() < 1024) {
-                    $('.main_container > .row > .right_col_wrapper').addClass('offset-2');
+                if ($(window).width() < 992) {
+                    $('.main_container > .row > .right_col_wrapper').addClass('offset-1');
                 } else {
                     $('.main_container > .row > .right_col_wrapper').removeClass('offset-md-2').addClass('offset-md-1');
                 }
@@ -137,8 +137,8 @@ $(document).ready(function () {
             $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
             $('.main_container > .row > .left_col').removeClass('col-lg-1 col-2 col-md-1').addClass('col-lg-2 col-md-2');
             if ($('.main_container > .row > .left_col.menu_fixed').length) {
-                if ($(window).width() < 1024) {
-                    $('.main_container > .row > .right_col_wrapper').removeClass('offset-2');
+                if ($(window).width() < 992) {
+                    $('.main_container > .row > .right_col_wrapper').removeClass('offset-1');
                 } else {
                     $('.main_container > .row > .right_col_wrapper').removeClass('offset-1').addClass('offset-md-2');
                 }
