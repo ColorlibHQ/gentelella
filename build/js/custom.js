7e50095f0ba615234368718bd3cc156d0d47598f
razvanaldea89 <razvan.aldea89@yahoo.ro>
2019-02-25 10:52:39 +0200

    build minified js

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,5 +1,40 @@
 /**
  * Resize function without multiple trigger
+ * 
+ * Usage:
+ * $(window).smartresize(function(){  
+ *     // code here
+ * });
+ */
+(function($,sr){
+    // debouncing function from John Hann
+    // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+    var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+        return function debounced () {
+            var obj = this, args = arguments;
+            function delayed () {
+                if (!execAsap)
+                    func.apply(obj, args); 
+                timeout = null; 
+            }
+
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
+
+            timeout = setTimeout(delayed, threshold || 100); 
+        };
+    };
+
+    // smartresize 
+    jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+/**
+ * Resize function without multiple trigger
  *
  * Usage:
  * $(window).smartresize(function(){
@@ -71,7 +106,7 @@ function setContentHeight() {
 };
 
 $(document).ready(function () {
-     $SIDEBAR_MENU.find('a').on('click', function (ev) {
+    $SIDEBAR_MENU.find('a').on('click', function (ev) {
         var $li = $(this).parent();
         var child_menu = $('ul:first', $li);
         var other_list_items = $li.parent().find('li').not($li);
