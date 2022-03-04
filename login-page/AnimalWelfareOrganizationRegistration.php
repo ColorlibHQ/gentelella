c4a134dd9786c78d6507b43f03369909f1ed2c2f
NielTatad <95074374+Genesisbadi@users.noreply.github.com>
2022-03-04 15:11:26 +0800

    NOV

--- login-page/AnimalWelfareOrganizationRegistration.php
+++ login-page/AnimalWelfareOrganizationRegistration.php
@@ -11,10 +11,10 @@ $Email=($_POST['Email']);
 $Username=($_POST['Username']);
 $Password=($_POST['Password']);
 
-$sql="INSERT INTO register(orgName,orgManager,Image,contactNo,Address,Email,Username,Password,Role,registerDate)VALUES(:Firstname,:Lastname,'default_logo.png',:ContactNo,:Address,:Email,:Username,:Password,'Animal Welfare Organization',Now())";
+$sql="INSERT INTO register(orgName,orgManager,Image,contactNo,Address,Email,Username,Password,Role,registerDate)VALUES(:OrganizationName,:OrganizationManager,'default_logo.png',:ContactNo,:Address,:Email,:Username,:Password,'Animal Welfare Organization',Now())";
 $query=$dbh->prepare($sql); 
-$query->bindParam(':Firstname',$OrganizationName,PDO::PARAM_STR);
-$query->bindParam(':Lastname',$OrganizationManager,PDO::PARAM_STR);
+$query->bindParam(':OrganizationName',$OrganizationName,PDO::PARAM_STR);
+$query->bindParam(':OrganizationManager',$OrganizationManager,PDO::PARAM_STR);
 $query->bindParam(':ContactNo',$ContactNo,PDO::PARAM_STR);
 $query->bindParam(':Address',$Address,PDO::PARAM_STR);
 $query->bindParam(':Email',$Email,PDO::PARAM_STR);
@@ -99,7 +99,6 @@ echo "<script type ='text/javascript'> document.location='http://localhost/devel
   <div class="d-lg-flex half">
     <div class="bg order-1 order-md-2" style="background-image: url('images/doggy.jpg');"></div>
     <div class="contents order-2 order-md-1">
-<<<<<<< HEAD
 
       
       <div class="row align-items-center justify-content-center">  
@@ -107,22 +106,12 @@ echo "<script type ='text/javascript'> document.location='http://localhost/devel
           <br>
             <form class="login100-form validate-form" method="post">
             <h6 style="text-align:center;margin-top:-14px;margin-right:-15px;">ANIMAL WELFARE ORGANIZATION <strong>REGISTRATION</strong></h6>
-=======
-      <div class="container">      
-          <div class="col-md-7">
-            <form class="login100-form validate-form" style=" width: 50vw; margin-left : -3vw;" method="post" enctype="multipart/form-data">
-              <br>
-              <br>
-              <br>
-					      <p style="text-align:center;"><img src="images/Logo.png" style="width:250px;height:250px;margin-top:-80px;margin-left:-15px;" alt=" " class="img-responsive"/></p>
-                  <h3 style="text-align:center;margin-top:-40px;">Animal Welfare Organization <strong>Registration:</strong></h3>
->>>>>>> 790f2911161e5d8314a5cb8df5dc1053174914ae
 					<br>
 					<div style="text-align: center" class="wrap-input100 validate-input">
-						<input class="input100" style="background-color:#f1f1f1;width:250px;height:40px;border:none;" type="text" name="Firstname" required="required" placeholder="Firstname">
+						<input class="input100" style="background-color:#f1f1f1;width:250px;height:40px;border:none;" type="text" name="OrganizationName" required="required" placeholder="Organization Name">
 					</div><br>
                     <div style="text-align: center" class="wrap-input100 validate-input">
-						<input class="input100" style="background-color:#f1f1f1;width:250px;height:40px;border:none;" type="text" name="Lastname" required="required" placeholder="Lastname">
+						<input class="input100" style="background-color:#f1f1f1;width:250px;height:40px;border:none;" type="text" name="OrganizationManager" required="required" placeholder="Organization Manager">
 						<span class="focus-input100"></span>
 					</div><br>
 					<div  style="text-align: center" class="wrap-input100 validate-input">
