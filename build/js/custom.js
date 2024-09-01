282b55a56bd3c2bcb82b0cc383af40dd68c13049
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-09-02 00:01:58 +0530

    Update custom.js

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,5 +1,5 @@
 /**
- * Resize function without multiple trigger
+ * Resize function without multiple triggers
  * 
  * Usage:
  * $(window).smartresize(function(){  
@@ -35,7 +35,7 @@
 })(jQuery,'smartresize');
 /**
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, choose Tools | Templates.
  * and open the template in the editor.
  */
 
