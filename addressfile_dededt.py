73d99c4389d264f934d6b5d52844da6903269fe5
danialzivehdar <243804543+danialzivehdadr@users.noreply.github.com>
2026-06-30 21:43:57 +0330

    Create addressfile_dededt.py

---
+++ addressfile_dededt.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# folder file add dede 
+UNWANTED_PATTERNS = [
+
+  Ebn Eein 
+   Play Photo on SD 
+    Play Video on Files;
+     Share [PDF, DOC, PPT, TXT, XLS, Files] 
+     to Eein DriveAddress on URL "Dededt";
+      Address Voices "Dededt";
+      Address on ID "Dededt";
+      Res "now";
+     Play Video on Veros 
+    Res "now" 
+    Share XLS "finance.xls"
+     to Eein Drive; 
+      Res "now";
+       Address on URL "Dededt" 
+        Res "now";
+       "status" : "ok", 
+       "action" : "Share",
+      "object" : ["PDF","DOC","PPT","TXT","XLS","Files" ],
+     "destination" : "Eein Drive",
+    "location" : "drive://eein/root/",
+    "result" : "5 files uploaded"
+]
